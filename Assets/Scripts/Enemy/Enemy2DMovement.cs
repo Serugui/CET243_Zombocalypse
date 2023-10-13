f6568679c133d0d9e362c7bef98164dac2c13545
Sergi Ramon <bi23tl@student.sunderland.ac.uk>
2023-10-13 10:49:37 +0100

    Enemy sytem added

--- Assets/Scripts/Enemy/Enemy2DMovement.cs
+++ Assets/Scripts/Enemy/Enemy2DMovement.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class Enemy2DMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody2D rb;
+    private Transform spriteTransform;
+    private PlayerAwarenessController playerAwarenessController;
+    private Vector2 targetDirection;
+    private Quaternion spriteInitialRotation; 
+
+    private void Awake()
     {
-        
+        rb = GetComponent <Rigidbody2D>();
+        spriteTransform = transform.GetChild(0); 
+        spriteInitialRotation = spriteTransform.rotation; 
+        playerAwarenessController = GetComponent<PlayerAwarenessController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        UpdateTargetDirection();
+        Flip();
+        ChasePlayer();
     }
+
+    private void UpdateTargetDirection()
+    {
+        if (playerAwarenessController.AwareOfPlayer)
+        {
+            targetDirection = playerAwarenessController.DirectionToPlayer;
+        }
+        else
+        {
+            targetDirection = Vector2.zero;
+        }
+    }
+
+    private void Flip()
+    {
+        if (rb.velocity.x >= 0)
+        {
+            spriteTransform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            spriteTransform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        if (targetDirection == Vector2.zero)
+        {
+            rb.velocity = Vector2.zero;
+        }
+        else
+        {
+            rb.velocity = targetDirection * speed;
+            RotateTowardsTarget(targetDirection);
+        }
+    }
+
+    private void RotateTowardsTarget(Vector2 direction)
+    {
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+        spriteTransform.rotation = spriteInitialRotation; 
+    }
+
 }
+
--- Assets/Scripts/Enemy/PlayerAwarenessController.cs
+++ Assets/Scripts/Enemy/PlayerAwarenessController.cs
@@ -1,18 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerAwarenessController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool AwareOfPlayer { get; private set; }
+    public Vector2 DirectionToPlayer { get; private set; }
+
+    [SerializeField]
+    private float playerAwarenessDistance;
+
+    private List<Transform> players = new List<Transform>();
+
+    private void Awake()
     {
-        
+        // Find all player objects in the scene and add them to the list
+        var playerObjects = GameObject.FindGameObjectsWithTag("Player");
+        foreach (var playerObject in playerObjects)
+        {
+            players.Add(playerObject.transform);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (players.Count == 0)
+        {
+            AwareOfPlayer = false;
+            DirectionToPlayer = Vector2.zero;
+            return;
+        }
+
+        Transform closestPlayer = FindClosestPlayer();
+
+        if (closestPlayer != null)
+        {
+            Vector2 enemyToPlayerVector = closestPlayer.position - transform.position;
+            DirectionToPlayer = enemyToPlayerVector.normalized;
+
+            if (enemyToPlayerVector.magnitude <= playerAwarenessDistance)
+            {
+                AwareOfPlayer = true;
+            }
+            else
+            {
+                AwareOfPlayer = false;
+            }
+        }
+    }
+
+    private Transform FindClosestPlayer()
+    {
+        Transform closestPlayer = null;
+        float closestDistance = float.MaxValue;
+
+        foreach (Transform player in players)
+        {
+            float distance = Vector3.Distance(transform.position, player.position);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestPlayer = player;
+            }
+        }
+
+        return closestPlayer;
     }
 }
+
